d535ae8fadce422f083f3a62e2dce2809e0c7427
LINHTRAN9x <tranvanlinh99x@gmail.com>
2023-12-09 10:03:30 +0700

    update

--- src/Flight/Flight.java
+++ src/Flight/Flight.java
@@ -1,13 +1,21 @@
 package Flight;
 
 public class Flight {
-    private int number = 0;
+    private int number;
     private String destination;
 
-    public Flight(){};
+    public Flight(){
+        this.number = 0;
+        this.destination = "";
+    };
     public Flight(int number, String destination) {
-        this.number = number;
-        this.destination = destination;
+        if (number > 0) {
+            this.number = number;
+            this.destination = destination;
+        } else {
+            this.number = 0;
+            this.destination = "";
+        }
     }
 
     public int getNumber() {
