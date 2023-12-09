36a83d912db0f8dcb6a4ddb0daf420d7dabbd0a3
LINHTRAN9x <tranvanlinh99x@gmail.com>
2023-12-09 10:00:19 +0700

    update

--- src/Flight/Main.java
+++ src/Flight/Main.java
@@ -3,6 +3,8 @@ package Flight;
 public class Main {
     public static void main(String[] args) {
         Flight g1 = new Flight(857,"Ha Noi");
+        Flight g2 = new Flight();
         g1.display();
+        g2.display();
     }
 }
